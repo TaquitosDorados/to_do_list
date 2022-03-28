d3043e760c639bd77fc267e5c4ce10e808e41e0b
TaquitosDorados <62762581+TaquitosDorados@users.noreply.github.com>
2022-03-28 10:54:10 -0600

    Lista Creada

--- src/components/To_Do/todo.js
+++ src/components/To_Do/todo.js
@@ -25,12 +25,53 @@ class todo extends Component {
             ],
         }
     }
+
+    handleOnChange = e => {
+        const {target:{value}} = e;
+        
+        this.setState({
+            task: value,
+        })
+    }
+
+    handleOnSubmit = e => {
+        e.preventDefault();
+
+        if(this.state.task.trim() != ''){
+            this.setState({
+                task: '',
+                items: [
+                    ...this.state.items,
+                    {
+                        id: uuidv4(),
+                        task: this.state.task,
+                        complete: false
+                    }
+                ]
+            })
+        }
+    }
+
     render() {
         
         return (
             console.log(this.state.items),
+            //console.log(this.state.task),
             <div className="Todo">
                 <h1>Nueva Tarea</h1>
+                <form onSubmit={this.handleOnSubmit}>
+                    <input type="text" value = {this.state.task} onChange = {this.handleOnChange}></input>
+                </form>
+
+                <ul>
+                    {
+                        this.state.items.map((item) => (
+                            <li>
+                                {item.task}
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         );
     }
